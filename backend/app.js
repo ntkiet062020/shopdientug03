739ffe015f4edf79021a0cbe5cfade07a0563409
ntkiet062020 <128658974+ntkiet062020@users.noreply.github.com>
2023-07-25 20:22:52 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
 const dotenv = require('dotenv');
 const path = require('path')
+const port = 3000; // Đặt cổng của bạn ở đây
 
 const errorMiddleware = require('./middlewares/errors')
 
@@ -35,13 +36,26 @@ app.use('/api/v1', auth)
 app.use('/api/v1', payment)
 app.use('/api/v1', order)
 
+// Cấu hình cho môi trường 'PRODUCTION'
 if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use('/', express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-    })
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+    });
+} else { // Môi trường phát triển
+    // Cấu hình để phục vụ các tệp nguồn (source files) của bạn trong thư mục frontend
+    app.use(express.static(path.join(__dirname, '../frontend')));
+
+    // Nếu bạn sử dụng React Router hoặc tương tự, bạn cần xử lý tất cả các đường dẫn ở phía máy chủ
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../frontend/index.html'));
+    });
 }
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 
 // Middleware to handle errors
