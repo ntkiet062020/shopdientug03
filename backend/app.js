804bfe53ecac1f621c7a231d6f21fe36e22cae00
ntkiet062020 <128658974+ntkiet062020@users.noreply.github.com>
2023-07-25 21:42:08 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,22 @@
 const express = require('express');
 const app = express();
 
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
-const path = require('path')
+const path = require('path');
 
-const errorMiddleware = require('./middlewares/errors')
+const errorMiddleware = require('./middlewares/errors');
 
-// Setting up config file 
+// Setting up config file
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
-dotenv.config({ path: 'backend/config/config.env' })
+dotenv.config({ path: 'backend/config/config.env' });
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(fileUpload());
-
-//Code thêm
-app.use(bodyParser.json({ limit: '10mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 
 // Import all routes
@@ -30,21 +26,21 @@ const payment = require('./routes/payment');
 const order = require('./routes/order');
 
 
-app.use('/api/v1', products)
-app.use('/api/v1', auth)
-app.use('/api/v1', payment)
-app.use('/api/v1', order)
+app.use('/api/v1', products);
+app.use('/api/v1', auth);
+app.use('/api/v1', payment);
+app.use('/api/v1', order);
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use('/login', express.static(path.join(__dirname, '../frontend/build')))
+    app.use('/login', express.static(path.join(__dirname, '../frontend/build')));
 
     app.get('login/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
     })
-}
+};
 
 
 // Middleware to handle errors
 app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
