15e6fd98466065539b2decd7059cb6ea109c341b
ntkiet062020 <128658974+ntkiet062020@users.noreply.github.com>
2023-07-25 19:41:06 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ app.use('/api/v1', payment)
 app.use('/api/v1', order)
 
 if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use('/', express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
