24452a5a4dea8fc0f8eff53bd8aea1fcc7849b6f
ntkiet062020 <128658974+ntkiet062020@users.noreply.github.com>
2023-07-25 22:25:03 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,41 @@
-const express = require('express');
-const app = express();
+const app = require('./app')
+const connectDatabase = require('./config/database')
 
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
 const dotenv = require('dotenv');
-const path = require('path');
+const cloudinary = require('cloudinary')
 
-const errorMiddleware = require('./middlewares/errors');
+// Handle Uncaught exceptions
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down due to uncaught exception');
+    process.exit(1)
+})
 
 // Setting up config file
 if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: 'backend/config/config.env' })
-dotenv.config({ path: 'backend/config/config.env' });
 
-app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser())
-app.use(fileUpload());
+dotenv.config({ path: 'backend/config/config.env' })
 
 
-// Import all routes
-const products = require('./routes/product');
-const auth = require('./routes/auth');
-const payment = require('./routes/payment');
-const order = require('./routes/order');
+// Connecting to database
+connectDatabase();
 
+// Setting up cloudinary configuration
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
 
-app.use('/api/v1', products);
-app.use('/api/v1', auth);
-app.use('/api/v1', payment);
-app.use('/api/v1', order);
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`)
+})
 
-if (process.env.NODE_ENV === 'PRODUCTION') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'));
+// Handle Unhandled Promise rejections
+process.on('unhandledRejection', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down the server due to Unhandled Promise rejection');
+    server.close(() => {
+        process.exit(1)
     })
-};
-
-
-// Middleware to handle errors
-app.use(errorMiddleware);
-
-module.exports = app;
+})
